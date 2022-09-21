0dd3c5803b65e76e38b645f36776844e8425b327
Anomander-R <orhan.sabanac@gmail.com>
2022-09-21 12:01:55 +0200

    Navbar completed

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bookmark-manager-tw
 Exercise in using Tailwind CSS utility classes 7/7. A clean and simple interface to organize your favorite website.
+
+
+## Colors
+
+Color Bucket: https://convertingcolors.com/color-bucket/view/5368DFFA57579194A1252B46.html
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '1020px',
+      xl:  '1440px'
+    },
+    extend: {
+      colors: {
+        softBlue: "#5368DF",
+        softRed: "#FA5757",
+        grayishBlue: "#9194A1",
+        veryDarkBlue: "#252B46"
+      },
+      fontFamily:{ 
+        sans: ['Rubik', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
